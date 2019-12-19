a3f53245a421d3c97c0aac2b7d48fa556edd1d10
Martin <martin.preusse@gmail.com>
2019-12-19 14:57:11 +0100

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='graphio',
-      version='0.0.1',
+      version='0.0.2',
       description='Library to load data sets to Neo4j.',
       url='http://github.com/kaiser_preusse',
       author='Martin Preusse',
