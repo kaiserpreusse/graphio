29c4c6fcf6154ddcc199363173e740bd9f67fd96
Martin <martin.preusse@gmail.com>
2019-12-20 16:48:45 +0100

    relative imports

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='graphio',
-      version='0.0.2',
+      version='0.0.3',
       description='Library to load data sets to Neo4j.',
       url='http://github.com/kaiser_preusse',
       author='Martin Preusse',
