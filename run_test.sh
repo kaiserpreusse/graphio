326cb6104a70f1ffab4155ac7740c1c6b5a7b4d4
Martin <martin.preusse@gmail.com>
2019-12-23 17:56:00 +0100

    add Docker to test, try deploy PyPI

---
+++ run_test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# run neo4j container
+docker run --name graphio_test_neo4j -p 7474:7474 -p 7473:7473 -p 7687:7687 --env NEO4J_AUTH=neo4j/test neo4j:latest
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='graphio',
-      version='0.0.4',
+      version='0.0.5',
       description='Library to load data sets to Neo4j.',
       url='http://github.com/kaiser_preusse',
       author='Martin Preusse',
