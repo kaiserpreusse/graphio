036fe7e511ac537bc41ab4fe48c4e3fad3ffd75e
Martin <martin.preusse@gmail.com>
2020-01-09 12:49:34 +0100

    bump version to 0.0.7

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='graphio',
-      version='0.0.6',
+      version='0.0.7',
       description='Library to load data sets to Neo4j.',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -24,6 +25,6 @@ setup(name='graphio',
           'Programming Language :: Python',
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
-            'License :: OSI Approved :: Apache Software License'
+          'License :: OSI Approved :: Apache Software License'
       ],
       )
