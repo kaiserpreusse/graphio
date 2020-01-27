4b721a2779227843170d78c6b79ff91c508e2a9c
Martin <martin.preusse@gmail.com>
2020-01-27 14:52:28 +0100

    test in multiple Neo4j versions

--- test/conftest.py
+++ test/conftest.py
@@ -13,31 +13,44 @@ CONTAINER_NAME = 'neo4j_graphio_test_run'
 
 NEO4J_PASSWORD = 'test'
 
+NEO4J_VERSIONS = [
+    {'version': '3.5', 'ports': (7474, 7473, 7687)},
+    {'version': '4.0', 'ports': (8474, 8473, 8687)}
+]
+
 
 @pytest.fixture(scope='session', autouse=True)
-def run_neo4j(graph):
+def run_neo4j():
     log.debug("Run Docker container.")
     client = docker.from_env()
 
-    existing_containers = client.containers.list(all=True, filters={'name': CONTAINER_NAME})
-    if existing_containers:
-        container = existing_containers[0]
-        container.start()
+    containers = []
 
-    else:
-        container = client.containers.run(image='neo4j:3.5',
-                                          ports={'7474/tcp': 7474, '7473/tcp': 7473, '7687/tcp': 7687},
+    # run all Neo4j containers
+    for v in NEO4J_VERSIONS:
+        container = client.containers.run(image='neo4j:{}'.format(v['version']),
+                                          ports={'7474/tcp': v['ports'][0], '7473/tcp': v['ports'][1], '7687/tcp': v['ports'][2]},
                                           environment={'NEO4J_AUTH': 'neo4j/{}'.format(NEO4J_PASSWORD)},
-                                          name=CONTAINER_NAME,
-                                          detach=True)
+                                          name='{}_{}'.format(CONTAINER_NAME, v['version']),
+                                          detach=True,
+                                          remove=True,
+                                          auto_remove=True)
 
+        containers.append(container)
+
+    # check availability for both containers
     connected = False
     max_retries = 20
     retries = 0
 
     while not connected:
         try:
-            graph.run("MATCH (n) RETURN n LIMIT 1")
+            # try to connect to both graphs, if it is not available `graph.run()` will
+            # throw a ServiceUnavailable error
+            for v in NEO4J_VERSIONS:
+                # get Graph, bolt connection to localhost is default
+                graph = Graph(password=NEO4J_PASSWORD, port=v['ports'][2])
+                graph.run("MATCH (n) RETURN n LIMIT 1")
             connected = True
 
         except ServiceUnavailable:
@@ -46,15 +59,16 @@ def run_neo4j(graph):
                 break
             sleep(1)
 
-    yield container
+    yield containers
 
     # stop container
-    container.stop()
+    for c in containers:
+        c.stop()
 
 
-@pytest.fixture(scope='session')
-def graph():
-    return Graph(password=NEO4J_PASSWORD)
+@pytest.fixture(scope='session', params=NEO4J_VERSIONS)
+def graph(request):
+    yield Graph(password=NEO4J_PASSWORD, port=request.param['ports'][2])
 
 
 @pytest.fixture
