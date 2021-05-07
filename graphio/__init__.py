fbcf04e029b57d028d1b7c8bf27a922fb8c758ab
Martin <martin.preusse@gmail.com>
2021-05-07 15:20:16 +0200

    top level import

--- graphio/__init__.py
+++ graphio/__init__.py
@@ -1,3 +1,5 @@
 from graphio.objects.nodeset import NodeSet
 from graphio.objects.relationshipset import RelationshipSet
 from graphio.objects.datacontainer import Container
+from graphio.model import ModelNode
+from graphio.model import ModelRelationship
