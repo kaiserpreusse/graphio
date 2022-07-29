0be5b4cab6987009b98453999f865f4cf1821fda
Martin Preusse <martin.preusse@gmail.com>
2022-07-29 12:45:01 +0200

    add test for queries

--- test/test_queries.py
+++ test/test_queries.py
@@ -1,5 +1,5 @@
-from graphio.queries import nodes_merge_unwind_preserve, nodes_merge_unwind_array_props, \
-    nodes_merge_unwind_preserve_array_props, CypherQuery
+from graphio.queries import nodes_create_unwind, nodes_merge_unwind, nodes_merge_unwind_preserve, nodes_merge_unwind_array_props, \
+    nodes_merge_unwind_preserve_array_props, rels_create_unwind, rels_merge_unwind, CypherQuery
 
 
 def test_cypher_query():
@@ -8,7 +8,24 @@ def test_cypher_query():
     assert c.query() == 'a\nb'
 
 
+class TestNodesCreateUnwind:
+
+    def test_nodes_create_unwind(self):
+        q = nodes_create_unwind(['Person'])
+        assert q == """UNWIND $props AS properties
+CREATE (n:Person)
+SET n = properties"""
+
+
 class TestNodesMergeUnwind:
+
+    def test_nodes_merge_unwind(self):
+        q = nodes_merge_unwind(['Person'], ['name'])
+
+        assert q == """UNWIND $props AS properties
+MERGE (n:Person { name: properties.name } )
+ON CREATE SET n = properties
+ON MATCH SET n += properties"""
 
     def test_nodes_merge_unwind_preserve(self):
         q = nodes_merge_unwind_preserve(['Person'], ['name'])
@@ -37,3 +54,25 @@ ON CREATE SET n = apoc.map.removeKeys(properties, $append_props)
 ON CREATE SET n.foo = [properties.foo], n.bar = [properties.bar]
 ON MATCH SET n += apoc.map.removeKeys(apoc.map.removeKeys(properties, $append_props), $preserve)
 ON MATCH SET n.foo = n.foo + properties.foo"""
+
+
+class TestRelsCreate:
+
+    def test_rels_create(self):
+        q = rels_create_unwind(['Person'], ['Movie'], ['name'], ['title'], "LIKES")
+        assert q == """UNWIND $rels AS rel
+MATCH (a:Person), (b:Movie)
+WHERE a.name = rel.start_name AND b.title = rel.end_title
+CREATE (a)-[r:LIKES]->(b)
+SET r = rel.properties RETURN count(r)"""
+
+
+class TestRelsMerge:
+
+    def test_rels_merge_unwind(self):
+        q = rels_merge_unwind(['Person'], ['Movie'], ['name'], ['title'], "LIKES")
+        assert q == """UNWIND $rels AS rel
+MATCH (a:Person), (b:Movie)
+WHERE a.name = rel.start_name AND b.title = rel.end_title
+MERGE (a)-[r:LIKES]->(b)
+SET r = rel.properties RETURN count(r)"""
