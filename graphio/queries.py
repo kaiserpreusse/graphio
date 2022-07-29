3013ba837c0ed2736b3040db99bb800826b53739
Martin Preusse <martin.preusse@gmail.com>
2022-07-29 12:44:40 +0200

    add queries

--- graphio/queries.py
+++ graphio/queries.py
@@ -36,6 +36,41 @@ def merge_clause(labels, merge_properties):
     return f"MERGE (n:{label_string} {{ {merge_clause_merge_properties(merge_properties)} }} )"
 
 
+def nodes_create_unwind(labels, property_parameter=None):
+    """
+    Generate a :code:`CREATE` query using :code:`UNWIND` for batch creation of nodes.::
+
+        UNWIND $props AS properties CREATE (n:Gene) SET n = properties
+
+    Pass the node properties as parameter to the query, e.g. with a :py:obj:`py2neo.Graph`::
+
+        graph.run(query, props=[{'id': 1}, {'id': 2}, ...])
+
+    You can optionally set the name of the parameter with the argument :code:`property_parameter`::
+
+        query = nodes_create_unwind(['Foo'], query_parameter='mynodes')
+
+        graph.run(query, mynodes=[{'id': 1}, {'id': 2}, ...])
+
+
+    :param labels: Labels for the create query.
+    :type labels: list[str]
+    :param property_parameter: Optional name of the parameter used in the query. Default is 'props'.
+    :type property_parameter: str
+    :return: Query
+    """
+    if not property_parameter:
+        property_parameter = 'props'
+
+    label_string = ":".join(labels)
+
+    q = CypherQuery(f"UNWIND ${property_parameter} AS properties",
+                    f"CREATE (n:{label_string})",
+                    "SET n = properties")
+
+    return q.query()
+
+
 def nodes_merge_unwind_preserve(labels, merge_properties, property_parameter=None):
     """
     Generate a :code:`MERGE` query which uses defined properties to :code:`MERGE` upon::
@@ -54,6 +89,59 @@ def nodes_merge_unwind_preserve(labels, merge_properties, property_parameter=Non
                     merge_clause(labels, merge_properties),
                     "ON CREATE SET n = properties",
                     "ON MATCH SET n += apoc.map.removeKeys(properties, $preserve)")
+
+    return q.query()
+
+
+def nodes_merge_unwind(labels, merge_properties, property_parameter=None):
+    """
+    Generate a :code:`MERGE` query which uses defined properties to :code:`MERGE` upon::
+
+        UNWIND $props AS properties
+        MERGE (n:Node {properties.sid: '1234'})
+        ON CREATE SET n = properties
+        ON MATCH SET n += properties
+
+    The '+=' operator in ON MATCH updates the node properties provided and leaves the others untouched.
+
+    Call with the labels and a list of properties to :code:`MERGE` on::
+
+        query = nodes_merge_unwind(['Foo', 'Bar'], ['node_id'])
+
+    Pass the node properties as parameter to the query, e.g. with a :py:obj:`py2neo.Graph`::
+
+        graph.run(query, props=[{'node_id': 1}, {'node_id': 2}, ...])
+
+    You can optionally set the name of the parameter with the argument :code:`property_parameter`::
+
+        query = nodes_merge_unwind([['Foo', 'Bar'], ['node_id'], query_parameter='mynodes')
+
+        graph.run(query, mynodes=[{'node_id': 1}, {'node_id': 2}, ...])
+
+
+    :param labels: Labels for the query.
+    :type labels: list[str]
+    :param merge_properties: Unique properties for the node.
+    :type merge_properties: list[str]
+    :param property_parameter: Optional name of the parameter used in the query. Default is 'props'.
+    :type property_parameter: str
+    :return: Query
+    """
+    if not property_parameter:
+        property_parameter = 'props'
+
+    label_string = ':'.join(labels)
+
+    merge_strings = []
+    for u in merge_properties:
+        merge_strings.append("{0}: properties.{0}".format(u))
+
+    merge_string = ', '.join(merge_strings)
+
+    q = CypherQuery(f"UNWIND ${property_parameter} AS properties",
+                    f"MERGE (n:{label_string} {{ {merge_string} }} )",
+                    "ON CREATE SET n = properties",
+                    "ON MATCH SET n += properties")
 
     return q.query()
 
@@ -125,5 +213,136 @@ def nodes_merge_unwind_preserve_array_props(labels, merge_properties, array_prop
 
     if on_match_array_props_list:
         q.append(f"ON MATCH SET {on_match_array_props_string}")
+
+    return q.query()
+
+
+def rels_params_from_objects(relationships, property_identifier=None):
+    """
+    Format Relationship properties into a one level dictionary matching the query generated in
+    `query_create_rels_from_list`. This is necessary because you cannot access nested dictionairies
+    in the UNWIND query.
+
+    UNWIND { rels } AS rel
+    MATCH (a:Gene), (b:GeneSymbol)
+    WHERE a.sid = rel.start_sid AND b.sid = rel.end_sid AND b.taxid = rel.end_taxid
+    CREATE (a)-[r:MAPS]->(b)
+    SET r = rel.properties
+
+    Call with params:
+        {'start_sid': 1, 'end_sid': 2, 'end_taxid': '9606', 'properties': {'foo': 'bar} }
+
+    :param relationships: List of Relationships.
+    :return: List of parameter dictionaries.
+    """
+    if not property_identifier:
+        property_identifier = 'rels'
+
+    output = []
+
+    for r in relationships:
+        d = {}
+        for k, v in r[0].items():
+            d['start_{}'.format(k)] = v
+        for k, v in r[1].items():
+            d['end_{}'.format(k)] = v
+        d['properties'] = r[2]
+        output.append(d)
+
+    return {property_identifier: output}
+
+
+def rels_create_unwind(start_node_labels, end_node_labels, start_node_properties,
+                             end_node_properties, rel_type, property_identifier=None):
+    """
+    Create relationship query with explicit arguments.
+
+    UNWIND $rels AS rel
+    MATCH (a:Gene), (b:GeneSymbol)
+    WHERE a.sid = rel.start_sid AND b.sid = rel.end_sid AND b.taxid = rel.end_taxid
+    CREATE (a)-[r:MAPS]->(b)
+    SET r = rel.properties
+
+    Call with params:
+        {'start_sid': 1, 'end_sid': 2, 'end_taxid': '9606', 'properties': {'foo': 'bar} }
+
+    Within UNWIND you cannot access nested dictionaries such as 'rel.start_node.sid'. Thus the
+    parameters are created in a separate function.
+
+    :param relationship: A Relationship object to create the query.
+    :param property_identifier: The variable used in UNWIND.
+    :return: Query
+    """
+
+    if not property_identifier:
+        property_identifier = 'rels'
+
+    start_node_label_string = ':'.join(start_node_labels)
+    end_node_label_string = ':'.join(end_node_labels)
+
+    q = CypherQuery()
+    q.append(f"UNWIND ${property_identifier} AS rel")
+    q.append(f"MATCH (a:{start_node_label_string}), (b:{end_node_label_string})")
+
+    # collect WHERE clauses
+    where_clauses = []
+    for property in start_node_properties:
+        where_clauses.append('a.{0} = rel.start_{0}'.format(property))
+    for property in end_node_properties:
+        where_clauses.append('b.{0} = rel.end_{0}'.format(property))
+
+    q.append("WHERE " + ' AND '.join(where_clauses))
+
+    q.append(f"CREATE (a)-[r:{rel_type}]->(b)")
+    q.append("SET r = rel.properties RETURN count(r)")
+
+    return q.query()
+
+
+def rels_merge_unwind(start_node_labels, end_node_labels, start_node_properties,
+                             end_node_properties, rel_type, property_identifier=None):
+    """
+    Merge relationship query with explicit arguments.
+
+    Note: The MERGE on relationships does not take relationship properties into account!
+
+    UNWIND $rels AS rel
+    MATCH (a:Gene), (b:GeneSymbol)
+    WHERE a.sid = rel.start_sid AND b.sid = rel.end_sid AND b.taxid = rel.end_taxid
+    MERGE (a)-[r:MAPS]->(b)
+    SET r = rel.properties
+
+    Call with params:
+        {'start_sid': 1, 'end_sid': 2, 'end_taxid': '9606', 'properties': {'foo': 'bar} }
+
+    Within UNWIND you cannot access nested dictionaries such as 'rel.start_node.sid'. Thus the
+    parameters are created in a separate function.
+
+    :param relationship: A Relationship object to create the query.
+    :param property_identifier: The variable used in UNWIND.
+    :return: Query
+    """
+
+    if not property_identifier:
+        property_identifier = 'rels'
+
+    start_node_label_string = ':'.join(start_node_labels)
+    end_node_label_string = ':'.join(end_node_labels)
+
+    q = CypherQuery()
+    q.append(f"UNWIND ${property_identifier} AS rel")
+    q.append(f"MATCH (a:{start_node_label_string}), (b:{end_node_label_string})")
+
+    # collect WHERE clauses
+    where_clauses = []
+    for property in start_node_properties:
+        where_clauses.append('a.{0} = rel.start_{0}'.format(property))
+    for property in end_node_properties:
+        where_clauses.append('b.{0} = rel.end_{0}'.format(property))
+
+    q.append("WHERE " + ' AND '.join(where_clauses))
+
+    q.append(f"MERGE (a)-[r:{rel_type}]->(b)")
+    q.append("SET r = rel.properties RETURN count(r)")
 
     return q.query()
