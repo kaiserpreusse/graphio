45b0c65703842c43a94e22971fb07fc40eacf839
Martin Preusse <martin.preusse@gmail.com>
2022-08-03 12:45:54 +0200

    run function

---
+++ graphio/graph.py
@@ -0,0 +1,17 @@
+from py2neo import Graph
+from neo4j import GraphDatabase, Driver, DEFAULT_DATABASE
+from typing import Union
+
+
+def run_query_return_results(connection:Union[Graph, Driver], query: str, database: str = None, **params):
+
+    if isinstance(connection, Graph):
+        result = list(connection.run(query, **params))
+        return result
+
+    elif isinstance(connection, Driver):
+        if not database:
+            database = DEFAULT_DATABASE
+        with connection.session(database=database) as s:
+            result = s.run(query, **params)
+            return list(result)
