93ccb77c25f2e84abdc995fa5be46477a8696492
Martin Preusse <martin.preusse@gmail.com>
2023-03-10 04:08:01 +0100

    all top level imports

--- graphio/__init__.py
+++ graphio/__init__.py
@@ -1,6 +1,6 @@
 from graphio.objects.nodeset import NodeSet
-from graphio.objects.unstructured_nodeset import UnstructuredNodeSet
-from graphio.objects.unstructured_relationshipset import UnstructuredRelationshipSet
+from graphio.objects.unstructured_nodeset import UnstructuredNodeSet, Node
+from graphio.objects.unstructured_relationshipset import UnstructuredRelationshipSet, Relationship
 from graphio.objects.relationshipset import RelationshipSet
 from graphio.objects.datacontainer import Container
 from graphio.model import ModelNode, ModelRelationship, MergeKey, Label, NodeDescriptor
