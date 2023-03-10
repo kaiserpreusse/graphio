18e1a6479b522855916fae5d3f779dff856a5860
Martin Preusse <martin.preusse@gmail.com>
2023-03-10 04:03:09 +0100

    import unstructured to top level

--- graphio/__init__.py
+++ graphio/__init__.py
@@ -1,4 +1,6 @@
 from graphio.objects.nodeset import NodeSet
+from graphio.objects.unstructured_nodeset import UnstructuredNodeSet
+from graphio.objects.unstructured_relationshipset import UnstructuredRelationshipSet
 from graphio.objects.relationshipset import RelationshipSet
 from graphio.objects.datacontainer import Container
 from graphio.model import ModelNode, ModelRelationship, MergeKey, Label, NodeDescriptor
