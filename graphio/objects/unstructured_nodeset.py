eda875c32559756689cda6893769a938280075d9
Martin Preusse <martin.preusse@gmail.com>
2023-03-10 03:16:56 +0100

    unstructured nodeset

---
+++ graphio/objects/unstructured_nodeset.py
@@ -0,0 +1,85 @@
+from typing import List
+
+from pydantic import BaseModel
+from neo4j import Driver, Transaction
+
+from graphio.queries import CypherQuery
+from graphio.helper import chunks, create_single_index, create_composite_index
+from graphio.queries import merge_clause_with_properties
+
+
+class Node(BaseModel):
+    labels: List[str]
+    merge_keys: List[str]
+    properties: dict = {}
+
+
+class UnstructuredNodeSet(BaseModel):
+    """
+    A set of nodes that do not have the same labels/merge keys.
+    """
+    nodes: List[Node] = []
+
+    def add_node(self, node: Node):
+        self.nodes.append(node)
+
+    @property
+    def unique_node_definitions(self):
+        """
+        Return a unqiue list of label/merge key combinations.
+        """
+        unique_nodes = set()
+
+        for node in self.nodes:
+            node_def = (tuple(node.labels), tuple(node.merge_keys))
+            if node_def not in unique_nodes:
+                unique_nodes.add(node_def)
+
+        return unique_nodes
+
+    def create_index(self, driver: Driver, database: str = None):
+        for labels, merge_keys in self.unique_node_definitions:
+            for label in labels:
+                for merge_key in merge_keys:
+                    create_single_index(driver, label, merge_key, database=database)
+                if len(merge_keys) > 1:
+                    create_composite_index(driver, label, merge_keys, database=database)
+
+    def create_nodes(self, tx, nodes: List[Node]):
+        for node in nodes:
+            q = CypherQuery(
+                f"CREATE (n:{':'.join(node.labels)})",
+                "SET n = $properties"
+            )
+            tx.run(q.query(), properties=node.properties)
+
+    def create(self, driver: Driver, database: str = None, batch_size=None):
+        """
+        Create all nodes in the set.
+        """
+        if not batch_size:
+            batch_size = 1000
+
+        with driver.session(database=database) as session:
+            for chunk in chunks(self.nodes, batch_size):
+                session.execute_write(self.create_nodes, chunk)
+
+    def merge_nodes(self, tx, nodes: List[Node]):
+        for node in nodes:
+            q = CypherQuery(
+                merge_clause_with_properties(node.labels, node.merge_keys, prop_name="$properties", node_variable="n"),
+                "SET n = $properties"
+            )
+            print(q.query())
+            tx.run(q.query(), properties=node.properties)
+
+    def merge(self, driver: Driver, database: str = None, batch_size=None):
+        """
+        Merge all nodes in the set.
+        """
+        if not batch_size:
+            batch_size = 1000
+
+        with driver.session(database=database) as session:
+            for chunk in chunks(self.nodes, batch_size):
+                session.execute_write(self.merge_nodes, chunk)
---
+++ test/test_objects/test_unstructured_nodeset.py
@@ -0,0 +1,123 @@
+from graphio.objects.unstructured_nodeset import UnstructuredNodeSet, Node
+from graphio.graph import run_query_return_results
+
+
+class TestUnstructuredNodeSet:
+
+    def test_unique_node_definitions(self):
+        uns = UnstructuredNodeSet()
+        uns.add_node(Node(labels=["A"], merge_keys=["a"]))
+        uns.add_node(Node(labels=["A"], merge_keys=["b"]))
+        uns.add_node(Node(labels=["B", "C"], merge_keys=["a"]))
+        uns.add_node(Node(labels=["B"], merge_keys=["b", "c"]))
+
+        assert uns.unique_node_definitions == {(('A',), ('b',)), (('B', 'C'), ('a',)), (('A',), ('a',)),
+                                               (('B',), ('b', 'c'))}
+
+
+class TestUnstructuredNodeSetIndexes:
+
+    def test_create_single_indexes(self, graph, clear_graph):
+        labels = ["A"]
+        properties = ["a"]
+        uns = UnstructuredNodeSet()
+        uns.add_node(Node(labels=labels, merge_keys=properties))
+
+        uns.create_index(graph)
+
+        # TODO keep until 4.2 is not supported anymore
+        try:
+            result = run_query_return_results(graph, "SHOW INDEXES YIELD *")
+        except:
+            result = run_query_return_results(graph, "CALL db.indexes()")
+
+        for row in result:
+            if 'tokenNames' in row:
+                assert row['tokenNames'] == labels and row['properties'] == properties \
+                       or row['tokenNames'] == labels and row['properties'] == properties
+
+            elif 'labelsOrTypes' in row:
+                assert row['labelsOrTypes'] == labels and row['properties'] == properties \
+                       or row['labelsOrTypes'] == labels and row['properties'] == properties
+
+    def test_create_composite_indexes(self, graph, clear_graph):
+        labels = ["A"]
+        properties = ["a", "b"]
+        uns = UnstructuredNodeSet()
+        uns.add_node(Node(labels=labels, merge_keys=properties))
+
+        uns.create_index(graph)
+
+        # TODO keep until 4.2 is not supported anymore
+        try:
+            result = run_query_return_results(graph, "SHOW INDEXES YIELD *")
+        except:
+            result = run_query_return_results(graph, "CALL db.indexes()")
+
+        for row in result:
+            if 'tokenNames' in row:
+                assert row['tokenNames'] == labels and row['properties'] == properties \
+                       or row['tokenNames'] == labels and row['properties'] == properties
+
+            elif 'labelsOrTypes' in row:
+                assert row['labelsOrTypes'] == labels and row['properties'] == properties \
+                       or row['labelsOrTypes'] == labels and row['properties'] == properties
+
+
+class TestUnstructuredNodeSetCreate:
+    def test_unstructured_nodeset_create(self, graph, clear_graph):
+        uns = UnstructuredNodeSet()
+        uns.add_node(Node(labels=["A"], merge_keys=["a"], properties={"a": 1}))
+        uns.add_node(Node(labels=["A"], merge_keys=["b"], properties={"b": 2}))
+        uns.add_node(Node(labels=["B", "C"], merge_keys=["a"], properties={"a": 3}))
+        uns.add_node(Node(labels=["B"], merge_keys=["b", "c"], properties={"b": 4, "c": 5}))
+
+        uns.create(graph)
+
+        result = run_query_return_results(graph, "MATCH (n) RETURN n, labels(n) AS labels")
+
+        assert len(result) == 4
+        for row in result:
+            if 'a' in row['n']:
+                if row['n']['a'] == 1:
+                    assert row['labels'] == ['A']
+            elif 'b' in row['n']:
+                if row['n']['b'] == 2:
+                    assert row['labels'] == ['A']
+                elif row['n']['b'] == 4:
+                    assert row['labels'] == ['B']
+                    assert row['n']['c'] == 5
+            elif 'c' in row['n']:
+                assert row['n']['c'] == 5
+                assert row['labels'] == ['B']
+
+
+class TestUnstructuredNodeSetMerge:
+    def test_unstructured_nodeset_merge(self, graph, clear_graph):
+        uns = UnstructuredNodeSet()
+        uns.add_node(Node(labels=["A"], merge_keys=["a"], properties={"a": 1}))
+        uns.add_node(Node(labels=["A"], merge_keys=["b"], properties={"b": 2}))
+        uns.add_node(Node(labels=["B", "C"], merge_keys=["a"], properties={"a": 3}))
+        uns.add_node(Node(labels=["B"], merge_keys=["b", "c"], properties={"b": 4, "c": 5}))
+
+        uns.merge(graph)
+        uns.merge(graph)
+        uns.merge(graph)
+
+        result = run_query_return_results(graph, "MATCH (n) RETURN n, labels(n) AS labels")
+
+        assert len(result) == 4
+
+        for row in result:
+            if 'a' in row['n']:
+                if row['n']['a'] == 1:
+                    assert row['labels'] == ['A']
+            elif 'b' in row['n']:
+                if row['n']['b'] == 2:
+                    assert row['labels'] == ['A']
+                elif row['n']['b'] == 4:
+                    assert row['labels'] == ['B']
+                    assert row['n']['c'] == 5
+            elif 'c' in row['n']:
+                assert row['n']['c'] == 5
+                assert row['labels'] == ['B']
