d67d901d4789243dead0adcc0dec87d1a1577aa1
Martin Preusse <martin.preusse@gmail.com>
2025-01-28 12:55:16 +0100

    update imports

--- graphio/__init__.py
+++ graphio/__init__.py
@@ -3,6 +3,4 @@ from graphio.objects.unstructured_nodeset import UnstructuredNodeSet, Node
 from graphio.objects.unstructured_relationshipset import UnstructuredRelationshipSet, Relationship, NodeMatch
 from graphio.objects.relationshipset import RelationshipSet, RelationshipSetDefinition
 from graphio.objects.datacontainer import Container
-from graphio.model import ModelNode, ModelRelationship, MergeKey, Label, NodeDescriptor
 from graphio.objects.properties import ArrayProperty
-from graphio.objects.update import GraphUpdate
