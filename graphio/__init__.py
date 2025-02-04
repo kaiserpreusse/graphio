1c3ff05ffb94c11efdeeee2dadd24490f32478b7
Martin Preusse <martin.preusse@gmail.com>
2025-02-04 18:53:02 +0100

    remove unstructured sets

--- graphio/__init__.py
+++ graphio/__init__.py
@@ -1,6 +1,4 @@
 from graphio.objects.nodeset import NodeSet
-from graphio.objects.unstructured_nodeset import UnstructuredNodeSet, Node
-from graphio.objects.unstructured_relationshipset import UnstructuredRelationshipSet, Relationship, NodeMatch
 from graphio.objects.relationshipset import RelationshipSet
 from graphio.objects.datacontainer import Container
 from graphio.queries import ArrayProperty
