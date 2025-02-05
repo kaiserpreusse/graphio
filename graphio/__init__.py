22bb95a435a3b9e9babee0e8949667e49b00a63c
Martin Preusse <martin.preusse@gmail.com>
2025-02-05 21:29:59 +0100

    remove container

--- graphio/__init__.py
+++ graphio/__init__.py
@@ -1,5 +1,5 @@
 from graphio.objects.nodeset import NodeSet
 from graphio.objects.relationshipset import RelationshipSet
-from graphio.objects.model import NodeModel, RelationshipModel, GraphModel, Container, Relationship, ModelBase, \
+from graphio.objects.model import NodeModel, RelationshipModel, GraphModel, Relationship, ModelBase, \
     model_initialize, model_create_index
 from graphio.queries import ArrayProperty
